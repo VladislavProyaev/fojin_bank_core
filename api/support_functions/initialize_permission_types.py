10c65257b0a27117ece2cfb4e21fd0d632801cfc
vproyaev <farmfilok@gmail.com>
2022-12-06 16:37:55 +0300

    update initialize_permission_types

--- api/support_functions/initialize_permission_types.py
+++ api/support_functions/initialize_permission_types.py
@@ -1,5 +1,6 @@
 from api.models.permission_type_model import PermissionTypeModel
 from common.constants.permissions import PermissionTypes
+from common.transaction import Transaction
 from services import sql
 
 
@@ -8,8 +9,8 @@ def initialize_permission_types():
     permission_type_model_count = sql.session.query(PermissionTypeModel).count()
 
     if permission_type_count != permission_type_model_count:
-        with sql.client.begin():
+        with Transaction(sql) as transaction:
             for permission_type in PermissionTypes.values():
                 PermissionTypeModel.get_or_create(
-                    sql, permission_type=permission_type
+                    transaction.sql, permission_type=permission_type
                 )
